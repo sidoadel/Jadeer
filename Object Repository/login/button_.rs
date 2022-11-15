528fbbd9576ed3077dd5eb22a5ca129032e9e034
andalosya <andalosya@192.168.1.14>
2022-11-15 10:31:16 +0200

    login TC

---
+++ Object Repository/login/button_.rs
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_</name>
+   <tag></tag>
+   <elementGuidId>2702c1c2-1080-4c4f-833d-dcec50fd2aa3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#PageContentContainer_SubmitBut</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='PageContentContainer_SubmitBut']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>2f255283-01d6-4227-b547-940020f7a070</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+      <webElementGuid>7ea8e8fa-e543-433a-a9e7-77b43514b6a0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tabindex</name>
+      <type>Main</type>
+      <value>4</value>
+      <webElementGuid>65dac803-6103-499d-af52-98b5cdb6a3d9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>ctl00$PageContentContainer$SubmitBut</value>
+      <webElementGuid>dfe37523-a05d-44eb-9fe5-f0c71ecc65c0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>value</name>
+      <type>Main</type>
+      <value>Login</value>
+      <webElementGuid>25714367-77a1-4525-9c8f-41351be3dcd7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>PageContentContainer_SubmitBut</value>
+      <webElementGuid>9c6a1986-1935-4332-9ecc-e1b458d278fd</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn fl-r-tr-right loginbtnht bg-primary-400 loginforms btn-labeled btn-labeled-right btn-block loginbtntxt pull-right-1 login-btn-arabic</value>
+      <webElementGuid>8f1ae461-aa99-466e-a634-dd22e71153cc</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                            
+                            تسجيل الدخول
+                        </value>
+      <webElementGuid>fd15ce6f-fe1e-4031-aa7e-6ae8a43d07e1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;PageContentContainer_SubmitBut&quot;)</value>
+      <webElementGuid>2c52e51f-1208-4102-9b5f-6ed88d76eaf3</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//button[@id='PageContentContainer_SubmitBut']</value>
+      <webElementGuid>d5fc0634-fa88-4923-832a-f6b33625e80d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='loginForm']/div/div[5]/div/button</value>
+      <webElementGuid>fbdc8c0c-d4cd-4f55-85b5-254b61cca5f3</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='التحقق'])[1]/following::button[1]</value>
+      <webElementGuid>66dacc64-e9ac-4c33-8762-a2c47cf0fac5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='الحقل كلمة المرور مطلوب'])[1]/following::button[1]</value>
+      <webElementGuid>61af1e77-2568-478f-af58-a1072f544d14</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//button</value>
+      <webElementGuid>779eb2a1-c2d1-41e5-9872-4ce4593fa330</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[@type = 'submit' and @name = 'ctl00$PageContentContainer$SubmitBut' and @id = 'PageContentContainer_SubmitBut' and (text() = '
+                            
+                            تسجيل الدخول
+                        ' or . = '
+                            
+                            تسجيل الدخول
+                        ')]</value>
+      <webElementGuid>a434b4f8-e223-4a68-8723-2bafd665aa4f</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
